86cc27fd3dad9b994b7698f425ae5d712966b548
Racksoup <Rack.connor1@gmail.com>
2021-07-16 21:31:52 -0400

    working filename in database upload image

--- models/HeaderImage.js
+++ models/HeaderImage.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const HeaderImageSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  price: { type: Number, required: true },
-  productImage: { type: String, required: true },
+  alt: { type: String, required: true },
+  headerimage: { type: String },
 });
 
 module.exports = HeaderImage = mongoose.model('headerImage', HeaderImageSchema);
--- routes/api/headerimage.js
+++ routes/api/headerimage.js
@@ -8,30 +8,79 @@ const HeaderImage = require('../../models/HeaderImage');
 
 const multer = require('multer');
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
 const upload = multer({
-  dest: 'upload/',
+  storage: storage,
 });
 
-router.get('/', (req, res) => {
-  HeaderImage.find({}, (err, items) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send('An error occurred', err);
-    } else {
-      res.render('imagesPage', { items: items });
-    }
-  });
-});
-
-router.post('/', upload.single('productImage'), async (req, res) => {
-  console.log(res.file);
+router.post('/', upload.single('headerimage'), async (req, res) => {
   const headerImage = new HeaderImage({
     name: req.body.name,
-    price: req.body.price,
+    alt: req.body.alt,
+    headerimage: req.file.path,
   });
+
   headerImage
     .save()
-    .then((result) => console.log(result))
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        message: 'Created HeaderImage successfully',
+        createdHeaderImage: {
+          name: result.name,
+          alt: result.alt,
+          _id: result._id,
+          request: {
+            type: 'GET',
+            url: 'http://localhost:3000/headerimages/' + result._id,
+          },
+        },
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
+
+router.get('/', (req, res, next) => {
+  HeaderImage.find()
+    .select('name price _id headerimage')
+    .exec()
+    .then((docs) => {
+      const response = {
+        count: docs.length,
+        headerimages: docs.map((doc) => {
+          return {
+            name: doc.name,
+            price: doc.price,
+            headerimage: doc.headerimage,
+            _id: doc._id,
+            request: {
+              type: 'GET',
+              url: 'http://localhost:3000/headerimages/' + doc._id,
+            },
+          };
+        }),
+      };
+      //   if (docs.length >= 0) {
+      res.status(200).json(response);
+      //   } else {
+      //       res.status(404).json({
+      //           message: 'No entries found'
+      //       });
+      //   }
+    })
     .catch((err) => {
       console.log(err);
       res.status(500).json({
