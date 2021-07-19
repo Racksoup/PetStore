b844b3889fb83221f8a03033463b7d686ad8b389
Racksoup <Rack.connor1@gmail.com>
2021-07-19 10:00:20 -0400

    Delete and Display image Routes

--- routes/api/headerimage.js
+++ routes/api/headerimage.js
@@ -73,6 +73,42 @@ router.get('/', (req, res) => {
   });
 });
 
+// @route GET /image/:filename
+// @desc Display Image
+router.get('/image/:filename', (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    // Check if file
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: 'No file exists',
+      });
+    }
+
+    // Check if image
+    if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+      // Read output to browser
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+    } else {
+      res.status(404).json({
+        err: 'Not an image',
+      });
+    }
+  });
+});
+
+// @route DELETE /files/:id
+// @desc  Delete file
+router.delete('/files/:id', (req, res) => {
+  gfs.remove({ _id: req.params.id, root: 'uploads' }, (err, gridStore) => {
+    if (err) {
+      return res.status(404).json({ err: err });
+    }
+
+    res.redirect('/');
+  });
+});
+
 module.exports = router;
 
 // ========================
