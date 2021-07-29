c82a2bee57a43de71a11df95ef53a4ad5c80185d
Racksoup <Rack.connor1@gmail.com>
2021-07-28 20:57:06 -0400

    Remove SIngleItem.js, Update Profile Screen

--- client/src/components/dashboard/UpdateProfile.js
+++ client/src/components/dashboard/UpdateProfile.js
@@ -1,10 +1,44 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Fragment } from 'react';
+import './Dashboard.css';
 
-const UpdateProfile = (props) => {
-  return <div>Hello</div>;
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+const UpdateProfile = ({ profile }) => {
+  return (
+    <Fragment>
+      <div className='ProfileTitleBox'>
+        <Link to='/dashboard'>
+          <button className='ProfileBackButton'>Back</button>
+        </Link>
+        <h3 className='ProfileTitle'>Profile</h3>
+        <button className='UpdateProfileButton'>Update Profile</button>
+      </div>
+      <div className='ProfileInfoBox'>
+        <div className='ProfileInfoItem'>
+          <p className='ProfileInfoTag'>Name:</p>
+          <p className='ProfileInfoValue'>{profile.name}</p>
+        </div>
+        <div className='ProfileInfoItem'>
+          <p className='ProfileInfoTag'>Email:</p>
+          <p className='ProfileInfoValue'>{profile.email}</p>
+        </div>
+        <div className='ProfileInfoItem'>
+          <p className='ProfileInfoTag'>Address:</p>
+          <p className='ProfileInfoValue'>{profile.address}</p>
+        </div>
+      </div>
+    </Fragment>
+  );
 };
 
-UpdateProfile.propTypes = {};
+UpdateProfile.propTypes = {
+  profile: PropTypes.object,
+};
 
-export default UpdateProfile;
+const mapStateToProps = (state) => ({
+  profile: state.profile.profile,
+});
+
+export default connect(mapStateToProps, {})(UpdateProfile);
