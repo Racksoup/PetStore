960afa55bdfc0417de40fa78e478a671c2e18ca8
Racksoup <Rack.connor1@gmail.com>
2021-08-08 14:56:59 -0400

    removed wrong route for deploy

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -81,4 +81,49 @@ router.post(
   }
 );
 
+router.post(
+  '/admin',
+  [
+    check('username', 'Please include a Username').exists(),
+    check('password', 'Pleace include a Password').exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { username, password } = req.body;
+
+    try {
+      let user = await User.findOne({ username });
+
+      if (!user) {
+        return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      console.log('adminToken');
+      jwt.sign(payload, config.get('adminSecret'), { expiresIn: '1h' }, (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
+
 module.exports = router;
