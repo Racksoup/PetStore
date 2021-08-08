b7b8e527731b5917e4f140bf0faa6f33c6bde84c
Racksoup <Rack.connor1@gmail.com>
2021-08-08 14:31:31 -0400

    ready for deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -16,6 +17,16 @@ app.use('/api/wishlist', require('./routes/api/wishlist'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/headerimage', require('./routes/api/headerimage'));
 app.use('/api/blogs', require('./routes/api/blogs'));
+
+// Server static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
